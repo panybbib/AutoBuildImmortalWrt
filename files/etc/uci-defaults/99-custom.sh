da43e72ed50a072ae7153a365e4477d2e37d3326
panybbib <pan.107@163.com>
2025-05-15 16:07:08 +0800

    Update 99-custom.sh

--- files/etc/uci-defaults/99-custom.sh
+++ files/etc/uci-defaults/99-custom.sh
@@ -11,7 +11,7 @@ uci set "dhcp.@domain[-1].ip=203.107.6.88"
 
 # LAN口设置静态IP
 uci set network.lan.proto='static'
-uci set network.lan.ipaddr='192.168.10.252'
+uci set network.lan.ipaddr='192.168.10.254'
 uci set network.lan.netmask='255.255.255.0'
 uci set network.lan.gateway='192.168.10.1'
 uci set network.lan.broadcast='192.168.10.255'
