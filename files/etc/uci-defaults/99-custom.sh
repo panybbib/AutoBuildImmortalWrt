32d588b11e03c7e16ba6677cfc4f50f7168579f4
panybbib <pan.107@163.com>
2025-05-20 23:28:11 +0800

    Update 99-custom.sh

--- files/etc/uci-defaults/99-custom.sh
+++ files/etc/uci-defaults/99-custom.sh
@@ -11,10 +11,10 @@ uci set "dhcp.@domain[-1].ip=203.107.6.88"
 
 # LAN口设置静态IP
 uci set network.lan.proto='static'
-uci set network.lan.ipaddr='192.168.2.254'
+uci set network.lan.ipaddr='192.168.2.1'
 uci set network.lan.netmask='255.255.255.0'
-uci set network.lan.gateway='192.168.2.1'
-uci set network.lan.broadcast='192.168.2.255'
+# uci set network.lan.gateway='192.168.2.1'
+# uci set network.lan.broadcast='192.168.2.255'
 
 # 设置所有网口可访问网页终端
 uci delete ttyd.@ttyd[0].interface
